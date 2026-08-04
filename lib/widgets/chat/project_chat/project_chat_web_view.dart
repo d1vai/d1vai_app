377c1c57bb9a82df2ca577fff469b974192e0234
aboutmydreams <aboutmydreams@163.com>
2026-08-05 05:49:26 +0800

    fix: apply release WebView settings to project preview

--- lib/widgets/chat/project_chat/project_chat_web_view.dart
+++ lib/widgets/chat/project_chat/project_chat_web_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
+import '../../inapp_webview_settings.dart';
+
 class ProjectChatWebView extends StatefulWidget {
   final String url;
 
@@ -12,35 +14,55 @@ class ProjectChatWebView extends StatefulWidget {
 
 class _ProjectChatWebViewState extends State<ProjectChatWebView>
     with AutomaticKeepAliveClientMixin {
-  late Widget _webView;
+  InAppWebViewController? _controller;
+  bool _hasError = false;
 
   @override
   bool get wantKeepAlive => true;
 
   @override
-  void initState() {
-    super.initState();
-    _webView = _buildWebView();
-  }
-
-  @override
   void didUpdateWidget(covariant ProjectChatWebView oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.url != widget.url) {
-      _webView = _buildWebView();
+      _hasError = false;
+      _controller?.loadUrl(urlRequest: URLRequest(url: WebUri(widget.url)));
     }
-  }
-
-  Widget _buildWebView() {
-    return InAppWebView(
-      contextMenu: ContextMenu(),
-      initialUrlRequest: URLRequest(url: WebUri(widget.url)),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return RepaintBoundary(child: _webView);
+    return RepaintBoundary(
+      child: Stack(
+        children: [
+          InAppWebView(
+            contextMenu: ContextMenu(),
+            initialUrlRequest: URLRequest(url: WebUri(widget.url)),
+            initialSettings: buildAppWebViewSettings(),
+            onWebViewCreated: (controller) => _controller = controller,
+            onLoadStart: (controller, url) {
+              if (mounted) setState(() => _hasError = false);
+            },
+            onReceivedError: (_, request, error) {
+              if (request.isForMainFrame != true || !mounted) return;
+              setState(() => _hasError = true);
+            },
+          ),
+          if (_hasError)
+            Positioned.fill(
+              child: ColoredBox(
+                color: Theme.of(context).colorScheme.surface,
+                child: Center(
+                  child: OutlinedButton.icon(
+                    onPressed: () => _controller?.reload(),
+                    icon: const Icon(Icons.refresh_rounded),
+                    label: const Text('Retry'),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
